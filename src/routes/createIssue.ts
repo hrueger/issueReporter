78c57d1dd609468d4f18a78c0570548413671db0
Hannes Rüger <hannesrueger@gmx.de>
2020-11-01 12:48:02 +0100

    added issue creation logic

--- src/routes/createIssue.ts
+++ src/routes/createIssue.ts
@@ -1,4 +1,15 @@
+import { Octokit } from "@octokit/rest";
 import { getGlobalConfigs } from "../globalConfigs";
+const octokit = new Octokit({
+    auth: getGlobalConfigs().token,
+    log: {
+        debug: () => console.debug,
+        info: () => console.info,
+        warn: console.warn,
+        error: console.error
+    },
+    
+});
 
 function getCreateIssueRoute(request, response) {
     response.render('createIssue',
@@ -8,7 +19,7 @@ function getCreateIssueRoute(request, response) {
     );
 }
 
-function postCreateIssueRoute(request, response) {
+async function postCreateIssueRoute(request, response) {
     const globals = getGlobalConfigs();
     if (!(request.body.title && request.body.description && typeof request.body.title == "string" && typeof request.body.description == "string")) {
         response.render('createIssue',
@@ -39,14 +50,40 @@ function postCreateIssueRoute(request, response) {
         );
     } else {
         // Submit issue
+        if (globals.repository) {
+            try {
+
+                await octokit.issues.create({
+                    owner: globals.repository.split("/")[0],
+                    repo: globals.repository.split("/")[1],
+                    title: request.body.title,
+                    body: request.body.description,
+                });
+                response.render('createIssue',
+                    {
+                        ...getGlobalConfigs(),
+                        issueCreatedSuccessfully: true,
+                    }
+                );
+            } catch {
+                error();
+            }
+        } else {
+            error();
+        }
+    }
+
+
+    function error() {
         response.render('createIssue',
             {
                 ...getGlobalConfigs(),
-                issueCreatedSuccessfully: true,
+                title: request.body.title,
+                description: request.body.description,
+                unknownError: true,
             }
         );
     }
-
 }
 
 export {
