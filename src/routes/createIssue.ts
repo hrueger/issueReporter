12aa687c29ed3d1bc2c4f110f3ae503e91626437
Hannes Rüger <hannesrueger@gmx.de>
2020-11-01 11:49:42 +0100

    added anti spam secret

--- src/routes/createIssue.ts
+++ src/routes/createIssue.ts
@@ -9,19 +9,29 @@ function getCreateIssueRoute(request, response) {
 }
 
 function postCreateIssueRoute(request, response) {
+    const globals = getGlobalConfigs();
     if (!(request.body.title && request.body.description && typeof request.body.title == "string" && typeof request.body.description == "string")) {
         response.render('createIssue',
             {
-                ...getGlobalConfigs(),
+                ...globals,
                 title: request.body.title,
                 description: request.body.description,
                 missingFields: true,
             }
         );
+    } else if (!(typeof request.body.secret == "string" && request.body.secret == globals.spamSecret)) {
+        response.render('createIssue',
+            {
+                ...globals,
+                title: request.body.title,
+                description: request.body.description,
+                secretIncorrect: true,
+            }
+        );
     } else if (!(request.body.title.length > 15 && request.body.description.length > 50)) {
         response.render('createIssue',
             {
-                ...getGlobalConfigs(),
+                ...globals,
                 title: request.body.title,
                 description: request.body.description,
                 tooShort: true,
